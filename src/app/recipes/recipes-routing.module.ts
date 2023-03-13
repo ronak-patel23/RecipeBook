b3a7187ecb4b281211b747a0c70dbe4bac747707
RONAK MANGROLIYA <ronakpatel2378@gmail.com>
2023-03-13 18:36:58 +0530

    shoppingList module and Shared Module added

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from '../auth/auth.component';
 import { AuthGuard } from '../auth/auth.guard';
 import { RecipeResolverResolver } from '../recipe-resolver.resolver';
-import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipesComponent } from './recipes.component';
