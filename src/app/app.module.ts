561b51beee285656bacf2082e3afe283817166b0
RONAK MANGROLIYA <ronakpatel2378@gmail.com>
2023-03-14 10:30:29 +0530

    core module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
 import { FormsModule } from '@angular/forms';
 import { ShoppingService } from './shopping-list/shopping.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; 
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { AlertComponent } from './shared/alert/alert.component';
-
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
-
 @NgModule({
   declarations: [AppComponent, HeaderComponent, AuthComponent],
   imports: [
@@ -28,7 +24,7 @@ import { SharedModule } from './shared/shared.module';
     ReactiveFormsModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [
     ShoppingService,
